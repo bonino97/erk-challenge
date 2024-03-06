562a6762f72815e4bd238a8edde19b9fb623fde3
bonino97 <juanbonino97@gmail.com>
2024-03-06 01:22:35 +0100

    warn: Comment files that contains credentials

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,5 @@
+// THIS FILE IS FOR TESTING PURPOSES ONLY AND SHOULD NEVER CONTAINS CREDENTIALS
+
 export const API_URL =
   (import.meta.env.VITE_API_URL as string) ||
   'https://kabsa.yallababy.com/api/v1';
